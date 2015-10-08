7cd5e18f255760f3d3d67609c0c71e7537858982
bfrieds <friedenberg.brian@gmail.com>
2015-10-07 18:57:44 -0700

    updated 10.7.15

--- camera_preview.py
+++ camera_preview.py
@@ -3,84 +3,117 @@ from picamera import PiCamera
 import time
 import cv2
 
-numPics = 0
-numVids = 0
-filename = 'test'
-title = 'Test'
+numPics = 0 #change this to the number of pictures that have already been made
+numVids = 0 #change this to the number of videos that have already been made
+filename = 'test'  #DONOT put the filetype in the filename
+
+pictureFileType = '.png' #sets the picture file type
+videoFileType = '.avi' #sets the video file type
+
+title = 'Test' #The title displayed on the video/photo
+
 fileTypeChosen = False
 video = False
 picture = False
 recording = False
 pics = []
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640,480))
+
+camera = PiCamera() #initializes the camera
+camera.resolution = (640, 480) #resolution for the camera.. too high and the RPi cannot display the image. Can go higher if not displaying the image. 
+camera.framerate = 32 #sets the framerate for the camera
+
+### camera settings ###
+#camera.shutter_speed = 
+#camera.sharpness = 
+#camera.saturation = 
+#camera.iso = 
+#camera.image_effect = 
+#camera.contrast = 
+#camera.brightness = 
+
+
+rawCapture = PiRGBArray(camera, size=camera.resolution)
 
 #allow the camera to warm up its sensor
 time.sleep(0.1)
 
 #capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
-     #grab the NumPy array of pixels
+    
     if not fileTypeChosen:
-        camera.annotate_text = 'click v for video mode \nor p for image mode'
+        #prompt user to select the mode
+        camera.annotate_text = 'click v for video mode \nor i for image mode'
     else:
+        #displays the title
         camera.annotate_text = title
 
+    #grab the NumPy array of pixels    
     image = frame.array
 
     #show the frame
     cv2.imshow(title, image)
+    
+    #wait a minimum of one milisecond, record if key pressed
     key = cv2.waitKey(1) & 0xFF
 
     #clear the stream
     rawCapture.truncate(0)
 
     if key == ord('q'):
-        rawCapture.close()
+        #quits the script
+        #TODO fix the closing mechanism
+        camera.close()
         cv2.destroyAllWindows()
         break
     elif not fileTypeChosen:
         if key == ord('v'):
-            print 'video'
+            print 'video mode'
             fileTypeChosen = True
             video = True
-        elif key == ord('p'):
-            print 'picture'
+        elif key == ord('i'):
+            print 'image mode'
             fileTypeChosen = True
             picture = True
     elif key == ord('r'):
+        #allows user to switch modes
         pics = []
         print 'reset'
         video = False
         picture = False
         fileTypeChosen = False
-    elif video == True:
-        pics += [image]
+    elif video == True: #video mode
+        pics += [image] #adds the current image to list of images
         if key == 32: #spacebar
             if recording == True:
+                # end the recording and save the video
                 print 'saving'
                 if numVids == 0:
-                    addon = ''
+                    addon = '' #no addon needed
                 else:
-                    addon = '_'+str(numVids+1)
+                    addon = '_'+str(numVids+1) #addon the number of videos already made so it doesn't override a video
                 height, width, layers = pics[0].shape
-                videowriter = cv2.VideoWriter(filename+addon+'.avi', 0, 32, (width, height), 1)
+                
+                #construct a video writer
+                videowriter = cv2.VideoWriter(filename+addon+videoFileType, 0, 32, (width, height), 1) 
+                
+                #loop through the pics and 'write' it to the video writer object                
                 for pic in pics:
                     videowriter.write(pic)
+                    
+                #'releases' the video writer and save the video
                 videowriter.release()
-                pics = []
-                numVids += 1
+                
+                pics = [] #resets the picture array to empty
+                numVids += 1 #adds one to the number of videos created
                 recording = False
             else:
                 print 'recording'
                 recording = True          
-    elif picture == True:
-        if key == 32: # spacebar
+    elif picture == True: #picture mode
+        if key == 32: #spacebar
             if numPics == 0:
                 addon = ''
             else:
                 addon = '_'+str(numPics+1)
             numPics += 1
-            cv2.imwrite(filename+addon+'.png', image)
+            cv2.imwrite(filename+addon+pictureFileType, image)
--- video_player.py
+++ video_player.py
@@ -1,45 +1,59 @@
 import cv2
-import numpy as np
 import os
+import numpy as np
 
-Folder = 'Videos'
-File = 'test.avi'
-SaveAs = 'testVidSave'
+Folder = 'Videos' #Folder relative to the file of the video
+File = 'HTAB_2mM_150ums_VaryConcentration_trial5.avi' #File Name of the video
+SaveAs = 'testVidSave' #What to save the pictures as
 
 play = False
+reverse = False
 numSaves = 0
 
 cd = os.getcwd()
 FilePath = os.path.join(cd, Folder, File)
 
 Video = cv2.VideoCapture(FilePath)
-num = 0
-Video.set(1, 100)
+num = 500
+Video.set(1, num) #set the video to a certain frame, does not work #TODO fix
+playBackSpeed = 4
+
+#retVal is a boolean that is true on a succesful read of the next frame
 retVal, frame = Video.read()
+
 while retVal == True:
     cv2.imshow('pic', frame)
-    if play:
-        num += 1
-        key = cv2.waitKey(1) & 0xFF #waits for one millisecond
-    else:
-        key = cv2.waitKey(0) & 0xFF #waits indefinitely
 
+    if play:
+        num += playBackSpeed
+        key = cv2.waitKey(1) & 0xFF #waits a minimum of one millisecond
+    elif reverse:
+        num -= playBackSpeed
+        key = cv2.waitKey(1) & 0xFF
+    else:        
+        key = cv2.waitKey(0) & 0xFF #waits indefinitely
         
     if key == ord('p'):
         play = not play
-    elif key == 81: #left arrow
-        num -= 5 # goes to previous frame
-    elif key == 83: #right arrow
-        num += 1
-    elif key == ord('s'):
+    if key == ord('r'):
+        reverse = not reverse
+    elif key == ord('q'):
+        Video.release()
+        cv2.destroyAllWindows()
+    elif key == ord('a'): #key a      
+        num -= 1 #goes to previous frame
+    elif key == ord('d'): #key d
+        num += 1 #goes to the next frame
+    elif key == ord('s'): #saves the image
         if numSaves == 0:
             addon = ''
         else:
             addon = '_'+str(numSaves)
         cv2.imwrite(SaveAs+addon+'.png', frame)
         numSaves += 1
-
+    
     Video.set(1, num)
-    retVal, frame = Video.read()
+    retVal, frame = Video.read() #read the next frame
 
-cv2.destroyAllWindows()
+Video.release()
+cv2.destroyAllWindows() #closes all the windows.. does not seem to work
